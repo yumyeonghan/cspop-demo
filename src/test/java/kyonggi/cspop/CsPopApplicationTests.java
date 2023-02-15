32733595825a9e8a378da4b0fce70ca2cf2b8549
yumyeonghan <mgtmh991013@gmail.com>
2023-02-16 01:54:34 +0900

    feat: 프로젝트 생성
    Spring Boot 3.0.2
    Packaging War
    Java 17

---
+++ src/test/java/kyonggi/cspop/CsPopApplicationTests.java
@@ -0,0 +1,13 @@
+package kyonggi.cspop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsPopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
