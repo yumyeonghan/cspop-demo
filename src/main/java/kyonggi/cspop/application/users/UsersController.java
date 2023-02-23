db8a72797046172a96ab4dba00006e5201e1f590
yumyeonghan <mgtmh991013@gmail.com>
2023-02-24 00:20:22 +0900

    feat: 브랜치 생성을 위한 /api/user/login API 생성
    - 테스트를 위한 API

--- src/main/java/kyonggi/cspop/application/users/UsersController.java
+++ src/main/java/kyonggi/cspop/application/users/UsersController.java
@@ -31,4 +31,9 @@ public class UsersController {
         usersService.checkDuplicateStudentNumber(studentId.get("studentId"));
         return ResponseEntity.noContent().build();
     }
+
+    @PostMapping("/user/login")
+    public ResponseEntity<Void> login () {
+        return ResponseEntity.noContent().build();
+    }
 }
