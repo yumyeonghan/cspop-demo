8bdd735939cf12453d4ee014e2e43bbad2f2f6cd
yumyeonghan <mgtmh991013@gmail.com>
2023-02-24 03:32:34 +0900

    feat: 로그아웃 기능 구현
    - 로그아웃 기능 구현
      - Post, api/logout
      - 해당 API 호출시 세션 정보 삭제

--- src/main/java/kyonggi/cspop/application/login/LoginController.java
+++ src/main/java/kyonggi/cspop/application/login/LoginController.java
@@ -4,6 +4,7 @@ import kyonggi.cspop.application.login.dto.LoginDto;
 import kyonggi.cspop.domain.login.LoginService;
 import kyonggi.cspop.domain.login.dto.UserSessionDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,12 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.Objects;
 
 import static kyonggi.cspop.application.SessionFactory.CSPOP_SESSION_KEY;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(("/api"))
+@Slf4j
 public class LoginController {
     private final LoginService loginService;
 
@@ -26,6 +29,17 @@ public class LoginController {
         UserSessionDto userSession = loginService.login(loginDto.getStudentId(), loginDto.getStudentPassword());
         HttpSession session = request.getSession();
         session.setAttribute(CSPOP_SESSION_KEY, userSession);
+        return "/api/home";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+
+        if(Objects.nonNull(session)) {
+            session.invalidate();
+        }
+
         return "/api/home";
     }
 }
