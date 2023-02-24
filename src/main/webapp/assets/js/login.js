cf6fdf0ef3f83d058a9a66d2935a58c0b7a05af8
HamHyeonJun <hyunjun003@naver.com>
2023-02-25 01:56:43 +0900

    refactor: login.js 리팩토링

--- src/main/webapp/assets/js/login.js
+++ src/main/webapp/assets/js/login.js
@@ -1,3 +1,6 @@
+/*
+* login.jsp 사용
+* */
 function login() {
     let account = {
         studentId: $(`#id`).val(),
@@ -9,37 +12,32 @@ function login() {
         data: JSON.stringify(account),
         contentType: "application/json; charset=utf-8",
         dataTypes: "json",
-        success: function (url) {
+        success: (url) => {
             alert("로그인 성공")
             window.location.replace(url);
-        }, error: function (error) {
-            alert(error.responseJSON.errorMessage);
-        }
+        }, error: (error) => alert(error.responseJSON.errorMessage)
+
     })
 }
-
-$(function userCheck(){
+/*
+* index.jsp 에서 사용
+* */
+$(function userCheck() {
     let userCheckButton = $(`#userCheck`)
-    let button="";
-    if (userId === "NotLogin"){
-        button += `<a href="/api/login"><button class="btn btn-primary btn-sm float-right">Login</button></a>`
-    }
-    else{
-        button = `<button class="btn btn-primary btn-sm float-right" onclick="logout()">Logout</button>`
-    }
+    let button = userId === "NotLogin"
+        ? `<a href="/api/login"><button class="btn btn-primary btn-sm float-right">Login</button></a>`
+        : `<button class="btn btn-primary btn-sm float-right" onclick="logout()">Logout</button>`;
     userCheckButton.append(button);
 })
 
-function logout(){
+function logout() {
     $.ajax({
-        url:"/api/logout",
+        url: "/api/logout",
         type: "POST",
-        success:(url)=>{
+        success: (url) => {
             alert("로그아웃 되었습니다")
             window.location.replace(url)
-        },error:(error)=>{
-            alert(error.responseJSON.errorMessage);
-        }
+        }, error: (error) => alert(error.responseJSON.errorMessage)
     })
 }
 
