106325674c069c6318282a2c506b0c10d42f1f28
yumyeonghan <mgtmh991013@gmail.com>
2023-02-24 18:09:32 +0900

    refactor: 컴파일 시 콘솔, warn 경고 해결
    - OSIV 측면에서 고려하여 spring.jpa.open-in-view=false 설정
    - UserDetailsService 타입의 빈이 등록이 되어있어야지 스프링 부트가 기본으로 생성해주는 유저가 더 이상 등록되지 않기 때문에 해당 빈 등록

--- src/main/java/kyonggi/cspop/domain/users/service/UsersService.java
+++ src/main/java/kyonggi/cspop/domain/users/service/UsersService.java
@@ -7,6 +7,9 @@ import kyonggi.cspop.exception.CsPopErrorCode;
 import kyonggi.cspop.exception.CsPopException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,7 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 @Slf4j
-public class UsersService {
+public class UsersService implements UserDetailsService {
 
     private final UsersRepository usersRepository;
     private final PasswordEncoder passwordEncoder;
@@ -36,5 +39,11 @@ public class UsersService {
         if (usersRepository.existsByStudentId(studentId)) {
             throw new CsPopException(CsPopErrorCode.DUPLICATE_STUDENT_ID);
         }
+    }
+
+    //사용 안함
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
     }
 }
