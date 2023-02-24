a2ce3c7a37d7215e743b6ff28577948988025abc
HamHyeonJun <hyunjun003@naver.com>
2023-02-25 01:39:11 +0900

    feat: session을 검사하여 login/logout 버튼 삽입 - 세션검사 후 button append하는 함수 구현 - logout 함수 생성

--- src/main/webapp/assets/js/login.js
+++ src/main/webapp/assets/js/login.js
@@ -1,6 +1,6 @@
-function login(){
+function login() {
     let account = {
-        studentId : $(`#id`).val(),
+        studentId: $(`#id`).val(),
         studentPassword: $(`#password`).val()
     }
     $.ajax({
@@ -9,11 +9,28 @@ function login(){
         data: JSON.stringify(account),
         contentType: "application/json; charset=utf-8",
         dataTypes: "json",
-        success:function (url){
+        success: function (url) {
             alert("로그인 성공")
             window.location.replace(url);
-        },error:function (error){
+        }, error: function (error) {
             alert(error.responseJSON.errorMessage);
         }
     })
 }
+
+$(function userCheck(){
+    let userCheckButton = $(`#userCheck`)
+    let button="";
+    if (userId === "NotLogin"){
+        button += `<a href="/api/login"><button class="btn btn-primary btn-sm float-right">Login</button></a>`
+    }
+    else{
+        button = `<button class="btn btn-primary btn-sm float-right" onclick="logout()">Logout</button>`
+    }
+    userCheckButton.append(button);
+})
+
+function logout(){
+    alert("logout!")
+}
+
