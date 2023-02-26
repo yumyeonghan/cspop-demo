d98175cf4b6566738330cc49fdb3db2df12b9ab6
euna0104 <koeuna0104@naver.com>
2023-02-26 23:10:41 +0900

    feat: 아이디, 비밀번호 검사 기능

--- src/main/webapp/assets/js/account.js
+++ src/main/webapp/assets/js/account.js
@@ -23,8 +23,10 @@ $(() => {
 function checkPw(pw1, pw2) {
     return pw1 !== pw2 ? 1 : 0;
 }
-
-
+// <아이디, 비밀번호 유효성 검사 여부 저장>
+let isId = false;
+let isPassword = false;
+let isPasswordConfirm = false;
 // 회원가입 함수
 function signUp() {
     let userData = {
@@ -38,6 +40,21 @@ function signUp() {
         classification: "학부생",
         department: $(`#major`).val()
     }
+
+    if($(`#id`).val() === ''){
+        return alert('아이디를 입력해주세요.');
+    }
+    //아이디는 숫자로만 받기(근데 학번으로)
+    const regExp1 = /[^0-9]{0,9}$/g;//정규식.숫자만 입력.9자리까지만
+    isId = $(`#id`).val()
+    if((false === regExp1.test(isId)){
+        alert('아이디가 형식에 맞지 않습니다.')
+    }
+    //9이하,9이상은 로그인 막아버리기
+    if ($(`#id`).length < 9 || $(`#id`).length > 9){
+        return alert("아이디가 형식에 맞지 않습니다.");
+    }
+
     // 패스워드 중복검사
     if (isCheckedId === 0 || isChangedId === 1) {
         alert("아이디 중복 확인을 해주세요");
@@ -56,6 +73,25 @@ function signUp() {
             })
         }
     }
+
+    if($(`#password`).val() === ''){
+        alert('비밀번호를 입력하세요');
+    }
+    if($(`#password2`).val() === ''){
+        alert('비밀번호 확인을 입력하세요');
+    }
+    const regExp = /^(?=.*?[a-zA-Z])(?=.*?[#?!@$ %^&*-]).{8,40}$/; //비밀번호 8자리 이상. 대문자. 특수문자 포함 정규식
+    isPassword = $(`#password`).val();
+    if((false === regExp.test(isPassword)){
+        alert('비밀번호가 형식에 맞지 않습니다.')
+    }
+    if($(`#password`).val() != $(`#password2`).val()){
+        alert('비밀번호와 비밀번호 확인이 같지 않습니다.');
+    }
+    if($(`#password`).val() === $(`#password2`).val()){
+            isPasswordConfirm = true;
+        }
+
 }
 
 //아이디 중복 확인 함수
