754ea653e7e85f5872e6211f532902cc9d4f10c6
yumyeonghan <mgtmh991013@gmail.com>
2023-03-19 01:57:32 +0900

    feat: schedule_board 테이블 데이터 추가

--- src/main/resources/sql/insert.sql
+++ src/main/resources/sql/insert.sql
@@ -5,6 +5,10 @@ INSERT INTO users VALUES (1, '2023-03-14 12:35:29.857156','2023-03-14 12:35:29.8
 --schedules 데이터
 INSERT INTO schedules VALUES (1,'2023-03-14 13:52:38.000000','2023-03-14 13:52:46.764849','2023-03-24','WAIT','2023-03-15','RECEIVED'),(2,'2023-03-14 13:52:38.000000','2023-03-14 13:52:50.544745','2023-03-14','PROCEEDING','2023-03-13','PROPOSAL'),(3,'2023-03-14 13:52:38.000000','2023-03-14 13:52:38.000000','2023-03-15','PROCEEDING','2023-03-13','INTERIM_REPORT'),(4,'2023-03-14 13:52:38.000000','2023-03-14 13:52:38.000000','2023-03-15','PROCEEDING','2023-03-13','FINAL_REPORT'),(5,'2023-03-14 13:52:38.000000','2023-03-14 13:52:38.000000','2023-03-15','PROCEEDING','2023-03-13','FINAL_PASS'),(6,'2023-03-14 13:52:38.000000','2023-03-14 13:52:38.000000','2023-03-15','PROCEEDING','2023-03-13','OTHER_QUALIFICATIONS');
 
+--schedule_board 데이터
+insert into cspop.schedule_board(id, created_date, last_modified_date, final_pass_text, final_report_text, interim_report_text, other_qualifications_text, proposal_text, received_text)
+VALUES (1,NOW(),NOW(),'내용','내용','내용','내용','내용','내용');
+
 --notice_board 데이터
 INSERT INTO `CSPOP`.`notice_board` (`id`, `created_date`, `last_modified_date`, `fixed`, `text`, `title`, `views`, `admins_id`) VALUES ('1', '1999-10-13', '1999-10-13', 1, 'text1', 'text1', 1, '1');
 INSERT INTO `CSPOP`.`notice_board` (`id`, `created_date`, `last_modified_date`, `fixed`, `text`, `title`, `views`, `admins_id`) VALUES ('2', '1999-10-13', '1999-10-13', 0, 'text1', 'text1', 1, '1');
